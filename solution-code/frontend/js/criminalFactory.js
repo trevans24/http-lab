8da3262568406f250c3e3e3fd2ff294f2e49c0a9
Stefan Kowal <kowalmax.s@gmail.com>
2017-04-20 14:43:28 -0600

    Revert "adding soln code"
    This reverts commit ab7694da0bed7dbf66cb49e0ef46f687e77575fb.

--- solution-code/frontend/js/criminalFactory.js
+++
@@ -1,8 +0,0 @@
-angular.module("criminalsApp")
-	.factory("Criminal", CriminalFactory);
-
-CriminalFactory.$inject = ["$resource"];
-
-function CriminalFactory($resource) {
-	return $resource("http://localhost:3000/criminals/:id");
-}
